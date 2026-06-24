b6870459a50955e08c2dc4124073b49425c02d58
Varshika-Gorentla <2024uec1793@mnit.ac.in>
2026-06-24 10:39:52 +0530

    Day-3 problem solved

---
+++ Basics/ReshapeTheMatrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <cstdint>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        // Check if reshape is possible
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        vector<vector<int>> ans(r, vector<int>(c));
+
+        for (int k = 0; k < m * n; k++) {
+            ans[k / c][k % c] = mat[k / n][k % n];
+        }
+
+        return ans;
+    }
+};
